a528d0399fa5480a25dca2ee8371ed0bc9b3052d
Davi Leal <davi.m.leal@gmail.com>
2023-01-02 22:29:49 -0300

    multi indiomas

---
+++ multi-indiomas/multi-indiomas/src/translate/languages/index.js
@@ -0,0 +1,9 @@
+import {messages as potugueseMessages} from './pt'
+import {messages as englishMessages} from './en'
+
+const messages= {
+    ...potugueseMessages,
+    ...englishMessages
+}
+
+export {messages}
---
+++ multi-indiomas/multi-indiomas/src/translate/languages/pt.js
@@ -0,0 +1,18 @@
+const messages = {
+    pt: {
+        translations:{
+            titles: {
+                app: 'Minha aplicacão'
+            },
+            messages: {
+                itWorks: 'Está funcionando com vários indiomas',
+                smallText: 'Um pequeno texto'
+            },
+            buttons: {
+                save: 'Salvar'
+            },
+        }
+    }
+}
+
+export {messages}
