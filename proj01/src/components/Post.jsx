3c47062ec6d8a31adff7cb9cc5ff496b60fb8bdb
davileal7 <108898208+davileal7@users.noreply.github.com>
2023-02-13 17:28:49 -0300

    axios

---
+++ proj01/src/components/Post.jsx
@@ -0,0 +1,9 @@
+export function Post({title, description}){
+    return (
+    <li>
+        <h2>{title}</h2>
+        <p>{description} </p>
+    </li>
+
+    )
+}
---
+++ proj01/src/components/Posts.jsx
@@ -0,0 +1,12 @@
+import { Post } from "./Post";
+import { posts } from "../posts.json";
+
+export function Posts() {
+    return (
+        <ul>
+            {posts.map((post) => {
+                return (<Post title={post.title} description={post.description}/>)   
+            })}
+        </ul>  
+    );  
+}
